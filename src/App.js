a11e9ac884a3d70954bcfea70c622865590cb032
horsellcommon <crawfordj716@gmail.com>
2022-10-31 16:43:36 +0000

    Added useEffect, useStates, fetched from API and included ternary operators to ensure that DOM does not update until API is fetched

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Sidebar from "./components/sidebar";
 
 function App() {
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const [images, setImages] = useState();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const fetchImages = async () => {
+    const response = await fetch("https://picsum.photos/v2/list");
+    const data = await response.json();
+    setImages(data);
+    setIsLoaded(true);
+  };
   return (
     <div className="App">
-      <h1>Test</h1>
+      {isLoaded && (
+        <div className="contained">
+          {images.map((item, index) => {
+            return (
+              <div>
+                <img src={item.download_url} height="200" width="200" />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
