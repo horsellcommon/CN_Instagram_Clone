31df31cdd84bce39b936f2e967e7ef055df5e213
horsellcommon <crawfordj716@gmail.com>
2022-10-31 15:31:08 +0000

    Added divs and extra content to sidebar.js

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,14 +1,38 @@
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <img className="bar-images" src="" alt="Home" />
-      <img className="bar-images" src="" alt="Search" />
-      <img className="bar-images" src="" alt="Explore" />
-      <img className="bar-images" src="" alt="Messages" />
-      <img className="bar-images" src="" alt="Notifications" />
-      <img className="bar-images" src="" alt="Create" />
-      <img className="bar-images" src="" alt="Profile" />
-      <img className="bar-images" src="" alt="More" />
+      <div className="bar-container">
+        <img className="bar-images" src="" alt="Home" />
+        <h2>Home</h2>
+      </div>
+      <div className="bar-container">
+        <img className="bar-images" src="" alt="Search" />
+        <h2>Search</h2>
+      </div>
+      <div className="bar-container">
+        <img className="bar-images" src="" alt="Explore" />
+        <h2>Explore</h2>
+      </div>
+      <div className="bar-container">
+        <img className="bar-images" src="" alt="Messages" />
+        <h2>Messages</h2>
+      </div>
+      <div className="bar-container">
+        <img className="bar-images" src="" alt="Notifications" />
+        <h2>Notifications</h2>
+      </div>
+      <div className="bar-container">
+        <img className="bar-images" src="" alt="Create" />
+        <h2>Create</h2>
+      </div>
+      <div className="bar-container">
+        <img className="bar-images" src="" alt="Profile" />
+        <h2>Profile</h2>
+      </div>
+      <div id="more-sidebar">
+        <img className="bar-images" src="" alt="More" />
+        <h2>More</h2>
+      </div>
     </div>
   );
 };
