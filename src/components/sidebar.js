e22e2dc61a3adc12df7a9de8f392dfc141af3d92
horsellcommon <crawfordj716@gmail.com>
2022-10-31 15:25:44 +0000

    Exported Sidebar and formatted sidebar.js

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,14 +1,16 @@
 const Sidebar = () => {
-    return(
-        <div className="sidebar">
-            <img className="bar-images" src="" alt="Home" />
-            <img className="bar-images" src="" alt="Search" />
-            <img className="bar-images" src="" alt="Explore" />
-            <img className="bar-images" src="" alt="Messages" />
-            <img className="bar-images" src="" alt="Notifications" />
-            <img className="bar-images" src="" alt="Create" />
-            <img className="bar-images" src="" alt="Profile" />
-            <img className="bar-images" src="" alt="More" />
-        </div>
-    )
-}
+  return (
+    <div className="sidebar">
+      <img className="bar-images" src="" alt="Home" />
+      <img className="bar-images" src="" alt="Search" />
+      <img className="bar-images" src="" alt="Explore" />
+      <img className="bar-images" src="" alt="Messages" />
+      <img className="bar-images" src="" alt="Notifications" />
+      <img className="bar-images" src="" alt="Create" />
+      <img className="bar-images" src="" alt="Profile" />
+      <img className="bar-images" src="" alt="More" />
+    </div>
+  );
+};
+
+export default Sidebar;
