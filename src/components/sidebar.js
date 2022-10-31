b257ea9f39fd5174ccc2e45d72eaaee97f99460f
horsellcommon <crawfordj716@gmail.com>
2022-10-31 15:25:11 +0000

    created components folder and sidebar.js - Added barebones code to sidebar.js

---
+++ src/components/sidebar.js
@@ -0,0 +1,14 @@
+const Sidebar = () => {
+    return(
+        <div className="sidebar">
+            <img className="bar-images" src="" alt="Home" />
+            <img className="bar-images" src="" alt="Search" />
+            <img className="bar-images" src="" alt="Explore" />
+            <img className="bar-images" src="" alt="Messages" />
+            <img className="bar-images" src="" alt="Notifications" />
+            <img className="bar-images" src="" alt="Create" />
+            <img className="bar-images" src="" alt="Profile" />
+            <img className="bar-images" src="" alt="More" />
+        </div>
+    )
+}
