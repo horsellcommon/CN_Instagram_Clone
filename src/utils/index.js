2039fc38e0e8d2b251e17fae0036a707431cdda1
horsellcommon <crawfordj716@gmail.com>
2022-11-01 15:17:30 +0000

    Read users added to utils/index

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,4 +18,19 @@ export const regUser = async (username, email, password, setter) => {
   }
 };
 
-export const readUsers = async () => {};
+export const readUsers = async () => {
+  try {
+    const response = await fetch("http://localhost:5001/read", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateUsers = async () => {};
+
+export const deleteUser = async () => {};
