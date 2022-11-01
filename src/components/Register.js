078139aa174bf255d84ed63cb60f3f966eca4948
horsellcommon <crawfordj716@gmail.com>
2022-11-01 10:27:30 +0000

    Register completed - added form, usestates

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,1 +1,31 @@
 import React from "react";
+import { useState } from "react";
+
+const Register = async ({ setter }) => {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label>
+        Username:
+        <input onChange={(e) => setUsername(e.target.value)} />
+      </label>
+
+      <label>
+        Email:
+        <input onChange={(e) => setEmail(e.target.value)} />
+      </label>
+
+      <label>
+        Password:
+        <input onChange={(e) => setPassword(e.target.value)} />
+      </label>
+
+      <button tpye="submit">Click here to register</button>
+    </form>
+  );
+};
+
+export default Register;
