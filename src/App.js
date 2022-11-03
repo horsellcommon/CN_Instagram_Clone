6e962a0fcfb46088ba91ba11662a28bd67007bd8
horsellcommon <crawfordj716@gmail.com>
2022-11-03 15:52:20 +0000

    Common folder and index.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Register</h1>
+      <Register setter={setUser} />
       <h1>Login</h1>
       <Login setter={setUser} />
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -68,4 +68,21 @@ export const deleteUser = async (username) => {
   }
 };
 
-export const loginUser = () => {};
+export const loginUser = async (username, email, password, setter) => {
+  try {
+      const response = await fetch("http://localhost:5001/login", {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify({
+              "username": username,
+              "email" : email,
+              "password" : password
+          })
+      })
+      const data = await response.json()
+      console.log(data)
+      setter(data.username)
+  } catch (error) {
+      console.log(error)
+  }
+}
