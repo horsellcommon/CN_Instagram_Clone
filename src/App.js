09fcf8910b726f3397d184ae2718ad64495c3c16
horsellcommon <crawfordj716@gmail.com>
2022-11-04 14:35:19 +0000

    Commented out import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import Sidebar from "./components/Sidebar";
+// import Sidebar from "./components/Sidebar";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import ReadUsers from "./components/ReadUsers";
